d936601c39126122e1f8291993a4acea24072104
AyanoNK <s.correa.wilches@gmail.com>
2025-04-18 15:17:44 -0500

    update com version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ayano.bank_sms_manager"
         minSdk = 31
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
